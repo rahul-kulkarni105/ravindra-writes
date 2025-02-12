6cbffb48b984e30cd350411f60bd4635a0842ff5
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2025-02-11 21:19:37 -0600

    feat: Initial commit - Add New article - Change deploy publish dir

--- app/routes/article.$slug.tsx
+++ app/routes/article.$slug.tsx
@@ -2,20 +2,17 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import ArticleView from "~/components/ArticleView";
 import { translateText } from "~/utils/translate.server";
+import articleData from "~/data/articleData.json";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const loader = async ({ params }: any) => {
   // Fetch the article from your database using params.slug
-  console.log('params', params)
-  const article = {
-    title: "पहिला लेख",
-    content: "हा एक मराठी लेख आहे.",
-  };
+  console.log('params', params);
 
-  const translatedTitle = await translateText(article.title, "mr", "en");
-  const translatedContent = await translateText(article.content, "mr", "en");
+  const translatedTitle = await translateText(articleData.title, "mr", "en");
+  const translatedContent = await translateText(articleData.content, "mr", "en");
 
-  return json({ ...article, translatedTitle, translatedContent });
+  return json({ ...articleData, translatedTitle, translatedContent });
 };
 
 export default function Article() {
