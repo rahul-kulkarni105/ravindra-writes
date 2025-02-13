1654b34857bc0cff42252d72c0f08c76fcbc5e52
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2025-02-12 22:52:37 -0600

    feat: - Added styles for index file to make it look presentable - Add css import with ? url - Add index.css file to app/styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,22 +1,29 @@
 import { useLoaderData, Link } from "@remix-run/react";
 import articleData from '~/data/articleData';
+import styles from '~/styles/index.css?url';
 
 export const loader = async () => {
   // Fetch articles from your database
-  const articles = articleData
+  const articles = articleData;
   return { articles };
 };
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function Index() {
   const { articles } = useLoaderData<typeof loader>();
 
   return (
-    <div>
+    <div className="container">
       <h1>Welcome to My Blog</h1>
-      <ul>
+      <ul className="article-list">
         {articles.map((article) => (
-          <li key={article.id}>
-            <Link to={`/article/${article.id}`}>{article.title}</Link>
+          <li key={article.id} className="article-item">
+            <Link to={`/article/${article.id}`} className="article-link">
+              {article.title}
+            </Link>
           </li>
         ))}
       </ul>
